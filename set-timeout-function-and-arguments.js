8315f217d0f29f06f2748b2d494685b222248a95
PanPey <piyushpandey615@gmail.com>
2022-08-17 11:01:05 +0530

    fourth commit

---
+++ set-timeout-function-and-arguments.js
@@ -0,0 +1,5 @@
+const fun1 = (param1, param2) => {
+    console.log(param1 + ' rocks!' + param2);
+};
+
+setTimeout(fun1, 2000, 'Node', ' and you?'); //3 and onwards arguments will act as parameters to the function above
